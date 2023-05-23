4d1df8f86836ec1813b1538e75ad4fcc15821b51
dbstn1325 <dbstn1325@naver.com>
2023-05-23 20:13:10 +0900

    order-service Init

---
+++ src/main/java/com/example/order/entity/Order.java
@@ -0,0 +1,39 @@
+package com.example.order.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@EntityListeners(AuditingEntityListener.class)
+@Table(name="orders")
+public class Order implements Serializable {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String userId;
+
+    @Column(nullable = false, length = 120, unique = true)
+    private String productId;
+    @Column(nullable = false)
+    private int stock;
+    @Column(nullable = false)
+    private int unitPrice;
+    @Column(nullable = false)
+    private int totalPrice;
+
+    @Column(nullable = false, unique = true)
+    private String orderId;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+}
